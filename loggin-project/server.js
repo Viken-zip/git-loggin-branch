b9e33f3a4b9dc785a439e686c82a34a3e9f8e32e
Viken-zip <assiviikman@gmail.com>
2022-01-26 12:34:34 +0200

    back-end working with data base

--- loggin-project/server.js
+++ loggin-project/server.js
@@ -1,4 +1,48 @@
+require('dotenv').config();
 const express = require('express');
 const server = express();
+const mongoose = require('mongoose');
 server.use(express.static('public'));
-server.listen(3000)
+server.use(express.urlencoded({extended:true}));
+const port = 3000;
+
+// dotenv database link
+mongoose.connect(process.env.database);
+ 
+const db = mongoose.connection;
+db.on('open', ()=>{
+    console.log('database connected');
+});
+
+// Schema for accounts
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema)
+
+server.post('/loggin', (req, res)=>{
+    console.log(req.body)
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+
+    async function find(){
+        User.exists({name: name, email: email, password: password}, function(err, result){
+            if(result === null){
+                console.log('no exist');
+                res.redirect('/'); // <= inloggnings sidå
+            } else {
+                console.log('exist');
+                res.redirect(''); // <= inglogga sido
+            }
+        })
+    }
+    find();
+})
+
+server.listen(port, ()=>{
+    console.log('servers on port: ' + port)
+});
